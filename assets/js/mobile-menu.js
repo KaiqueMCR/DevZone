0ea3075cbbc5442a6db3c98e7d674ec0433ddb3b
KaiqueMCR <kaiquedev404@gmail.com>
2022-05-26 12:35:06 -0300

    add some accessibility

--- assets/js/mobile-menu.js
+++ assets/js/mobile-menu.js
@@ -4,9 +4,23 @@ const navbar = document.querySelector('.navbar')
 mobileMenu.addEventListener('click', () => {
   mobileMenu.classList.toggle('active')
   navbar.classList.toggle('active')
+  if (mobileMenu.classList.contains('active')) {
+    mobileMenu.setAttribute('aria-label', 'Close menu')
+    navbar.setAttribute('aria-expanded', 'true')
+  } else {
+    mobileMenu.setAttribute('aria-label', 'Open menu')
+    navbar.setAttribute('aria-expanded', 'true')
+  }
 })
 
 navbar.addEventListener('click', () => {
   mobileMenu.classList.toggle('active')
   navbar.classList.toggle('active')
+  if (mobileMenu.classList.contains('active')) {
+    mobileMenu.setAttribute('aria-label', 'Close menu')
+    navbar.setAttribute('aria-expanded', 'true')
+  } else {
+    mobileMenu.setAttribute('aria-label', 'Open menu')
+    navbar.setAttribute('aria-expanded', 'true')
+  }
 })
